3e9f849045770503932d32696ba4d0f3e2473e9d
David Valdespino Pavon <david.valdespino@gases.co.cu>
2022-09-20 12:05:59 -0400

    Added executable icon support on windows

--- build.rs
+++ build.rs
@@ -1,7 +1,11 @@
 use std::io;
 
+#[cfg(windows)]
+extern crate windres;
+use windres::Build;
+
 fn main() -> io::Result<()> {
     // println!("cargo:rustc-link-lib=unarr");
-
+    Build::new().compile("resources.rc").unwrap();
     Ok(())
 }
